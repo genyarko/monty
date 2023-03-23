ad6fd186f70229ce7caf5175f6209ca1a7dff849
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-23 17:47:15 +0100

    Update main.c

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 	fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-	return EXIT_FAILURE;
+	exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
 	bus.file = file;
