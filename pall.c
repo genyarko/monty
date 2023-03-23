69621005b6581eaa907dd876f66b996c5f7765d0
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-23 10:29:24 +0100

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 /**
- * f_pall - prints the stack
+ * g_pall - prints the stack
  * @head: stack head
  * @counter: no used
  * Return: no return
 */
-void pall(stack_t **head, unsigned int counter)
+void g_pall(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 	(void)counter;
