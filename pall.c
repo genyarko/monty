6fcf33b243f9b06a1ce0a52f845d5e99cd356170
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-23 08:56:35 +0100

    Create pall.c

---
+++ pall.c
@@ -0,0 +1,17 @@
+/**
+  * pall - Prints all values on the stack, starting from the top
+  *
+  * Usage: pall
+  *
+  * Return: 0 on success, or EXIT_FAILURE on failure
+  */
+int main()
+{
+    printf("Printing all values in the stack:\n");
+
+    // Iterate over all elements in the stack
+
+    // Print each element
+
+    return (0);
+}
