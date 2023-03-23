37cd0f79a0c0b515a5f89bd210333d10705ad0d0
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-23 10:09:33 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -56,11 +56,8 @@ extern bus_t bus;
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
  */
-typedef struct instruction_s
-{
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
+
+void execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
 char  *clean_line(char *content);
