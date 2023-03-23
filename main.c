bfbcb9e0f0e683c5a0f8dde97008df1cdf4a1d62
genyarko <115112347+genyarko@users.noreply.github.com>
2023-03-23 18:19:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "Usage: %s monty file\n", argv[0]);
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
